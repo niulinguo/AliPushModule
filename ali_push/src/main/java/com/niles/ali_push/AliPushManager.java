bdfbe1a22b0e4a1eb3af4717d7386fe8b2215f34
Niles <niulinguo@163.com>
2018-10-23 18:53:50 +0800

    优化

--- ali_push/src/main/java/com/niles/ali_push/AliPushManager.java
+++ ali_push/src/main/java/com/niles/ali_push/AliPushManager.java
@@ -1,7 +1,10 @@
 package com.niles.ali_push;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 
 import com.alibaba.sdk.android.push.CloudPushService;
@@ -19,6 +22,32 @@ public class AliPushManager {
 
     public static void init(Application app) {
         initCloudChannel(app);
+        initNotificationChannel(app);
+    }
+
+    private static void initNotificationChannel(Application app) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager = (NotificationManager) app.getSystemService(Context.NOTIFICATION_SERVICE);
+            // 通知渠道的id
+            String id = "1";
+            // 用户可以看到的通知渠道的名字.
+            CharSequence name = "PushTest";
+            // 用户可以看到的通知渠道的描述
+            String description = "推送测试";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            // 配置通知渠道的属性
+            mChannel.setDescription(description);
+            // 设置通知出现时的闪灯（如果 android 设备支持的话）
+//            mChannel.enableLights(true);
+//            mChannel.setLightColor(Color.RED);
+            // 设置通知出现时的震动（如果 android 设备支持的话）
+//            mChannel.enableVibration(true);
+//            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            //最后在notificationmanager中创建该通知渠道
+            assert mNotificationManager != null;
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
     }
 
     /**
